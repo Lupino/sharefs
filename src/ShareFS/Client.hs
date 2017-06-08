4f8bacade8619d7addb60773ca2d55e02dd6ad77
Lupino <lmjubuntu@gmail.com>
2017-06-08 18:57:11 +0800

    update path join

--- src/ShareFS/Client.hs
+++ src/ShareFS/Client.hs
@@ -20,6 +20,10 @@ import           Network.Wreq
 import           ShareFS.Internal     (Gateway (..), initMgr)
 import           ShareFS.SimpleStat   (SimpleStat (..))
 import           ShareFS.Wreq
+import qualified System.FilePath      as FP (dropDrive, (</>))
+
+(</>) :: FilePath -> FilePath -> FilePath
+root </> path = root FP.</> (FP.dropDrive path)
 
 reqTS :: IO String
 reqTS = do
@@ -32,23 +36,23 @@ getFile path gw = do
   ts <- reqTS
   responseEither $ getWith (getOptions gw) (uri ++ ts)
 
-  where uri = getGWUri gw ++ "/file" ++ path
+  where uri = getGWUri gw ++ "/file" </> path
 
 -- put "/file/path/to/filename"
 putFile :: FilePath -> LB.ByteString -> Gateway -> IO (Either String ())
 putFile path dat gw = do
-  opts <- getOptionsAndSignRaw ("/file" ++ path) (LB.toStrict dat) gw
+  opts <- getOptionsAndSignRaw ("/file" </> path) (LB.toStrict dat) gw
   responseEither' $ putWith opts uri dat
 
-  where uri = getGWUri gw ++ "/file" ++ path
+  where uri = getGWUri gw ++ "/file" </> path
 
 -- delete "/file/path/to/filename"
 deleteFile :: FilePath -> Gateway -> IO (Either String ())
 deleteFile path gw = do
-  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/file" ++ path) )] gw
+  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/file" </> path) )] gw
   responseEither' $ deleteWith opts uri
 
-  where uri = getGWUri gw ++ "/file" ++ path
+  where uri = getGWUri gw ++ "/file" </> path
 
 -- get "/stat/path/to/filename_or_dir"
 statFile :: FilePath -> Gateway -> IO (Either String SimpleStat)
@@ -56,7 +60,7 @@ statFile path gw = do
   ts <- reqTS
   responseEitherJSON $ getWith (getOptions gw) (uri ++ ts)
 
-  where uri = getGWUri gw ++ "/stat" ++ path
+  where uri = getGWUri gw ++ "/stat" </> path
 
 -- get "/dir/path/to/dir"
 getDir :: FilePath -> Gateway -> IO (Either String [SimpleStat])
@@ -64,20 +68,20 @@ getDir path gw = do
   ts <- reqTS
   responseEitherJSON $ getWith (getOptions gw) (uri ++ ts)
 
-  where uri = getGWUri gw ++ "/dir" ++ path
+  where uri = getGWUri gw ++ "/dir" </> path
 
 -- put "/dir/path/to/dir"
 putDir :: FilePath -> Gateway -> IO (Either String ())
 putDir path gw = do
-  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/dir" ++ path) )] gw
+  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/dir" </> path) )] gw
   responseEither' $ putWith opts uri LB.empty
 
-  where uri = getGWUri gw ++ "/dir" ++ path
+  where uri = getGWUri gw ++ "/dir" </> path
 
 -- post "/rename/path/to/dir"
 renameFile :: FilePath -> FilePath -> Gateway -> IO (Either String ())
 renameFile src dst gw = do
-  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/rename" ++ src) ), ("dst", LT.pack dst)] gw
+  opts <- getOptionsAndSign [( "sign_path", LT.pack ("/rename" </> src) ), ("dst", LT.pack dst)] gw
   responseEither' $ postWith opts uri [ "dst" := dst ]
 
-  where uri = getGWUri gw ++ "/rename" ++ src
+  where uri = getGWUri gw ++ "/rename" </> src
